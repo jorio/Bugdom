75bd9b7295a60dc9e85c1a5d558e1e27a8757cd5
Iliyas Jorio <iliyas@jor.io>
2020-11-21 18:10:37 +0100

    Import backdrop from Nanosaur port

--- src/Headers/gamepatches.h
+++ src/Headers/gamepatches.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "GLBackdrop.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -9,6 +11,25 @@ TQ3StorageObject Q3FSSpecStorage_New(const FSSpec* spec);
 TQ3Area GetAdjustedPane(int windowWidth, int windowHeight, Rect paneClip);
 
 void DoSDLMaintenance(void);
+
+//-----------------------------------------------------------------------------
+// Backdrop
+
+void SetWindowGamma(int percent);
+
+void ExclusiveOpenGLMode_Begin(void);
+
+void ExclusiveOpenGLMode_End(void);
+
+void AllocBackdropTexture(void);
+
+void SetBackdropClipRegion(int width, int height);
+
+void ClearBackdrop(UInt32 argb);
+
+void RenderBackdropQuad(int fit);
+
+void DisposeBackdropTexture(void);
 
 #ifdef __cplusplus
 }
