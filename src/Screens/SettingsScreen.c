56778497ff0a23fb81e1ec7e7ac5cf005e1a93c1
Iliyas Jorio <iliyas@jor.io>
2020-12-23 14:55:52 +0100

    SettingsScreen: grass blade buttons

--- src/Screens/SettingsScreen.c
+++ src/Screens/SettingsScreen.c
@@ -35,6 +35,7 @@ static const TQ3ColorRGB gTextColor			= { 1.0f, 0.9f, 0.0f };
 static const TQ3ColorRGB gTitleTextColor	= { 1.0f, 0.9f, 0.0f };
 static const TQ3ColorRGB gDeleteColor		= { 0.9f, 0.3f, 0.1f };
 static const TQ3ColorRGB gBackColor			= { 1,1,1 };
+static const TQ3ColorRGB gValueTextColor	= { 190/255.0f,224/255.0f,0 };
 
 static char textBuffer[512];
 
@@ -259,6 +260,7 @@ static void NukeObjectsInSlot(int objNodeSlotID)
 		ObjNode* nextNode = node->NextNode;
 		if (node->Slot == objNodeSlotID)
 		{
+			//QD3D_ExplodeGeometry(node, 200.0f, PARTICLE_MODE_UPTHRUST | PARTICLE_MODE_NULLSHADER, 1, 0.5f);
 			DeleteObject(node);
 		}
 		node = nextNode;
@@ -267,8 +269,12 @@ static void NukeObjectsInSlot(int objNodeSlotID)
 
 static void MakeSettingEntryObjects(int settingID, bool firstTime)
 {
-	static const float XSPREAD = 100;
-	static const float LH = 16;
+	static const float XSPREAD = 120;
+	static const float LH = 28;
+
+	const float x = 0;
+	const float y = 80 - LH * settingID;
+	const float z = 0;
 
 	SettingEntry* entry = &gSettingEntries[settingID];
 
@@ -278,11 +284,12 @@ static void MakeSettingEntryObjects(int settingID, bool firstTime)
 		NukeObjectsInSlot(20000 + settingID);
 	}
 
+	bool hasSubtitle = entry->subtitle && *entry->ptr == entry->subtitleShownForValue;
+
 	TextMeshDef tmd;
 	TextMesh_FillDef(&tmd);
 
-	tmd.coord.x = 0;
-	tmd.coord.y += 80 - LH * settingID;
+	tmd.coord = (TQ3Point3D) {x,y,z};
 	tmd.align = TEXTMESH_ALIGN_LEFT;
 	tmd.scale = 0.3f;
 	tmd.slot = 10000 + settingID;
@@ -290,7 +297,24 @@ static void MakeSettingEntryObjects(int settingID, bool firstTime)
 	if (firstTime)
 	{
 		// Create pickable quad
-		PickableQuads_NewQuad(tmd.coord, XSPREAD*2.0f, LH*0.9f, settingID);
+		PickableQuads_NewQuad(
+				(TQ3Point3D) {x+XSPREAD/2, y, z},
+				XSPREAD*1.0f, LH*0.75f,
+				settingID);
+
+		// Create grass blade
+		gNewObjectDefinition.group 		= MODEL_GROUP_LEVELSPECIFIC2;
+		gNewObjectDefinition.type 		= LAWN2_MObjType_Grass2;
+		gNewObjectDefinition.coord		= (TQ3Point3D){x+XSPREAD, y-1, z-1};
+		gNewObjectDefinition.slot 		= 10000 + settingID;
+		gNewObjectDefinition.flags 		= STATUS_BIT_NULLSHADER;
+		gNewObjectDefinition.moveCall 	= nil;
+		gNewObjectDefinition.rot 		= 0;//PI+PI/2;
+		gNewObjectDefinition.scale 		= .02;
+		ObjNode* grassBlade = MakeNewDisplayGroupObject(&gNewObjectDefinition);
+		grassBlade->Rot.z = PI/2.0f;
+		grassBlade->Scale.z = 0.001f;
+		UpdateObjectTransforms(grassBlade);
 
 		// Create caption text
 		tmd.coord.x = -XSPREAD;
@@ -299,18 +323,21 @@ static void MakeSettingEntryObjects(int settingID, bool firstTime)
 	}
 
 	// Create value text
+	tmd.color = gValueTextColor;
 	tmd.coord.x = XSPREAD/2.0f;
+	if (hasSubtitle)
+		tmd.coord.y += LH * 0.15f;
 	if (entry->useClovers)
 	{
 		for (unsigned int i = 0; i < entry->nChoices; i++)
 		{
-			TQ3Point3D coord = tmd.coord;
+			TQ3Point3D coord = {x+XSPREAD/2, y, z};
 			coord.x += 12.0f * (i - (entry->nChoices-1)/2.0f);
 			gNewObjectDefinition.group 		= MODEL_GROUP_BONUS;
-			gNewObjectDefinition.type 		= BONUS_MObjType_BlueClover;
+			gNewObjectDefinition.type 		= BONUS_MObjType_GoldClover;
 			gNewObjectDefinition.coord		= coord;
 			gNewObjectDefinition.slot 		= 20000 + settingID;
-			gNewObjectDefinition.flags 		= 0;
+			gNewObjectDefinition.flags 		= STATUS_BIT_NULLSHADER;
 			gNewObjectDefinition.moveCall 	= nil;
 			gNewObjectDefinition.rot 		= PI+PI/2;
 			gNewObjectDefinition.scale 		= .03;
@@ -327,7 +354,7 @@ static void MakeSettingEntryObjects(int settingID, bool firstTime)
 	}
 
 	// Create subtitle text
-	tmd.coord.y -= LH / 2.0f;
+	tmd.coord.y -= LH * 0.15f * 2.0f;
 	if (entry->subtitle && *entry->ptr == entry->subtitleShownForValue)
 	{
 		float scaleBackup = tmd.scale;
@@ -413,6 +440,9 @@ static void SetupSettingsScreen(void)
 
 	FSMakeFSSpec(gDataSpec.vRefNum, gDataSpec.parID, ":models:BonusScreen.3dmf", &spec);
 	LoadGrouped3DMF(&spec,MODEL_GROUP_BONUS);
+
+	FSMakeFSSpec(gDataSpec.vRefNum, gDataSpec.parID, ":models:Lawn_Models2.3dmf", &spec);
+	LoadGrouped3DMF(&spec,MODEL_GROUP_LEVELSPECIFIC2);
 
 	TextMesh_Load();
 
