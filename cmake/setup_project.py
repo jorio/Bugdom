eb93b08f13b769ed791ebdc00f5204f4d9da4aac
Iliyas Jorio <iliyas@jor.io>
2020-12-28 09:45:26 +0100

    Nuke SDL mouse accel patch for macOS
    KillMacMouseAcceleration.c incoming instead

--- cmake/setup_project.py
+++ cmake/setup_project.py
@@ -14,8 +14,6 @@ from dataclasses import dataclass
 libs_dir = os.path.abspath('extern')
 cache_dir = os.path.abspath('cache')
 
-use_cocoa_mouse_fix = True
-
 sdl_ver = "2.0.14"
 
 #----------------------------------------------------------------
@@ -56,34 +54,6 @@ def nuke_if_exists(path):
 
 #----------------------------------------------------------------
 
-def build_sdl_with_cocoa_mouse_patch():
-    print("==== Building SDL with Cocoa mouse patch")
-    sdl_src_path = get_package(F"http://libsdl.org/release/SDL2-{sdl_ver}.zip")
-
-    with tempfile.TemporaryDirectory() as scratch_dir:
-        # Extract SDL source
-        sdl_srcdir = scratch_dir + F'/SDL2-{sdl_ver}'
-        with ZipFile(sdl_src_path, 'r') as zip:
-            zip.extractall(path=scratch_dir)
-
-        # Patch it
-        with open(F'extern/CocoaMouseFix-SDL2-{sdl_ver}.patch', 'r') as patchfile:
-            call(['patch', '--strip=1'], stdin=patchfile, cwd=sdl_srcdir)
-
-        # Build it
-        call(['xcodebuild',
-            '-quiet',
-            '-project', 'Xcode/SDL/SDL.xcodeproj',
-            '-configuration', 'Release',
-            '-scheme', 'Framework',
-            '-derivedDataPath', scratch_dir
-        ], cwd=sdl_srcdir)
-
-        # Copy SDL2.framework to extern
-        shutil.copytree(F'{scratch_dir}/Build/Products/Release/{sdl2_framework}', sdl2_framework_target_path, symlinks=True)
-
-#----------------------------------------------------------------
-
 # Make sure we're running from the correct directory...
 if not os.path.exists('src/Enemies/Enemy_WorkerBee.c'):  # some file that's likely to be from the Bugdom source tree
     print("\x1b[1;31mSTOP - Please run this script from the root of the Bugdom source repo\x1b[0m")
@@ -95,6 +65,8 @@ cmake_generated_dirs = {}
 cmake_extra_build_args = []
 
 if system == 'Windows':
+    nuke_if_exists(F'{libs_dir}/SDL2-{sdl_ver}')
+
     print("==== Fetching SDL")
     sdl_zip_path = get_package(F"http://libsdl.org/release/SDL2-devel-{sdl_ver}-VC.zip")
     with ZipFile(sdl_zip_path, 'r') as zip:
@@ -108,15 +80,12 @@ elif system == 'Darwin':
 
     nuke_if_exists(sdl2_framework_target_path)
 
-    if use_cocoa_mouse_fix:
-        build_sdl_with_cocoa_mouse_patch()
-    else:
-        printf("==== Fetching SDL")
-        sdl_dmg_path = get_package(F"http://libsdl.org/release/SDL2-{sdl_ver}.dmg")
-        with tempfile.TemporaryDirectory() as mount_point:
-            call(['hdiutil', 'attach', sdl_dmg_path, '-mountpoint', mount_point, '-quiet'])
-            shutil.copytree(F'{mount_point}/{sdl2_framework}', sdl2_framework_target_path, symlinks=True)
-            call(['hdiutil', 'detach', mount_point, '-quiet'])
+    print("==== Fetching SDL")
+    sdl_dmg_path = get_package(F"http://libsdl.org/release/SDL2-{sdl_ver}.dmg")
+    with tempfile.TemporaryDirectory() as mount_point:
+        call(['hdiutil', 'attach', sdl_dmg_path, '-mountpoint', mount_point, '-quiet'])
+        shutil.copytree(F'{mount_point}/{sdl2_framework}', sdl2_framework_target_path, symlinks=True)
+        call(['hdiutil', 'detach', mount_point, '-quiet'])
 
     cmake_generated_dirs['build-xcode'] = CMakeConfig(['-G', 'Xcode'], ['Release', 'Debug'])
     cmake_extra_build_args = ['--', '-quiet']
